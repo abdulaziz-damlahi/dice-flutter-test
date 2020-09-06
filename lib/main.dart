3970bd2e0577b24fc6c82ca60f1e9141586590c7
abdulaziz-damlahi <abood_d@live.com>
2020-09-06 21:25:37 +0300

    Button

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice2.png'),
+            child: FlatButton(
+              onPressed: () {
+                print('left');
+              },
+              child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('right');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
